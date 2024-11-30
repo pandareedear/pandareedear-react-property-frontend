88a18bd25384c3580ae1719e7e336a85d6d90a47
pandareedear <pandaree.c51@gmail.com>
2024-11-30 16:50:48 +0700

    test update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-    return <div className="text-2xl font-bold">HomePage</div>;
+    return (
+        <>
+            <div className="text-2xl font-bold">HomePage</div>
+            <div className="text-xl">search bar</div>
+            <div className="text-xl">content</div>
+        </>
+    );
 }
